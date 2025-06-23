c7dcb464cf853091c8f35742efccc123b37f9695
CulturedLeftFoot <clfenquiry@gmail.com>
2025-06-23 11:49:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -320,11 +320,11 @@ NCB St = (((Attributes[Hea]+Attributes[Tck]+Attributes[Agg]+Attributes[Bra]+Attr
 
         if top_n_players.empty:
         st.warning(f"No players are ranked in the top {rank_threshold} across all roles.")
-        else:
-            if display_option == "Scores":
+            else:
+                if display_option == "Scores":
             df_to_display = score_pivot.loc[top_n_players.index]
             st.dataframe(df_to_display.style.format("{:.2f}"), use_container_width=True)
-            else:
+                else:
             df_to_display = rank_pivot.loc[top_n_players.index]
             st.dataframe(df_to_display.style.format("{:.0f}"), use_container_width=True)
 else:
